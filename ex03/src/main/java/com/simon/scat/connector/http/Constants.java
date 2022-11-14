013d6e9339ac6002f7f3c4c26e49e653942759e9
Simon <ketteringkevin@sina.com>
2022-11-14 09:56:07 +0800

    ex03

---
+++ ex03/src/main/java/com/simon/scat/connector/http/Constants.java
@@ -0,0 +1,8 @@
+package com.simon.scat.connector.http;
+
+import java.io.File;
+
+public interface Constants {
+    String WEB_ROOT = System.getProperty("user.dir") + File.separator + "webroot";
+    String Package = "com.simon.scat.connector.http";
+}
